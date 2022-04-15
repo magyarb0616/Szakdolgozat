8000e5e47388f230a6d6064487c50cda56578c24
Magyar Bence <magyarb0616@gmail.com>
2022-04-15 14:09:33 +0200

    Revert "Revert "Completed all layers for Breeds and Species:""
    This reverts commit 81a482db0fe00f0da3d6390056092bd35aa4fb40.

---
+++ src/main/java/com/szakdolgozat/petadopt/DTO/SpeciesDTO.java
@@ -0,0 +1,22 @@
+package com.szakdolgozat.petadopt.DTO;
+
+public class SpeciesDTO {
+    private String name;
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
